8ba4f8ed838cf92156685fbdc26597e5c3d4945b
Alex Kravchenko <lyosha.kravchenko.2013@mail.ru>
2020-11-14 21:33:58 +0300

    Version 1.0

---
+++ SASPLab1/Models/WaffleContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace SASPLab1.Models
+{
+    public class WaffleContext : DbContext
+    {
+        public DbSet<Waffle> Waffles { get; set; }
+        public DbSet<Purchase> Purchases { get; set; }
+    }
+}
