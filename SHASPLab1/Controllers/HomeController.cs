108e0f5501e06caff9225fbacf14a40872a02c68
Alex Kravchenko <lyosha.kravchenko.2013@mail.ru>
2020-11-23 20:03:27 +0300

    Version 1.0

---
+++ SHASPLab1/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using SHASPLab1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SHASPLab1.Controllers
+{
+    public class HomeController : Controller
+    {
+        JuiceContext db = new JuiceContext();
+        public ActionResult Index()
+        {
+            IEnumerable<Juice> juices = db.Juices;
+            ViewBag.Juices = juices;
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Buy(int id)
+        {
+            ViewBag.JuiceId = id;
+            return View();
+        }
+
+        [HttpPost]
+        public string Buy(Purchase purchase)
+        {
+            purchase.Date = DateTime.Now;
+            db.Purchases.Add(purchase);
+            db.SaveChanges();
+            return "Спасибо," + purchase.Person + ", за покупку!";
+        }
+    }
+}
---
+++ SHASPLab1/Models/Juice.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SHASPLab1.Models
+{
+    public class Juice
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Maker { get; set; }
+        public DateTime ShelfLife { get; set; }
+        public int Price { get; set; }
+    }
+}
