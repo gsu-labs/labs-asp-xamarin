613cb24a4b2538a304d22f612fa944c47edb16a3
Alex Kravchenko <lyosha.kravchenko.2013@mail.ru>
2020-11-23 23:38:21 +0300

    Version 1.0

---
+++ SHXamLab2/SHXamLab2/Grouping.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace SHXamLab2
+{
+    public class Grouping<K, T> : ObservableCollection<T>
+    {
+        public K Name { get; private set; }
+        public Grouping(K name, IEnumerable<T> items)
+        {
+            Name = name;
+            foreach (T item in items)
+            {
+                Items.Add(item);
+            }
+        }
+    }
+}
---
+++ SHXamLab2/SHXamLab2/Movie.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SHXamLab2
+{
+    public class Movie
+    {
+        public string Name { get; set; }
+        public string Genre { get; set; }
+        public int Price { get; set; }
+        public DateTime Date { get; set; }
+        public string ImagePath { get; set; }
+    }
+}
